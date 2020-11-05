dd8b1fa6869c45e5639956f7b4fbce1100994855
nikitha26 <mnikitha365@gmail.com>
2020-11-05 17:37:32 +0530

    basic things created

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Icon from './components/Icon';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Icon/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Icon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {FaTimes ,FaPen, FaRegCircle} from "react-icons/fa";
+
+function Icon(){
+    return(
+         <h4>
+             <FaTimes className="icon"/>
+             <FaPen className="icon"/>
+             <FaRegCircle className="icon"/>
+         </h4>
+    )
+}
+export default Icon;
