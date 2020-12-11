59ebd09aced440307ca36d411cc666dca3210e95
nikitha26 <mnikitha365@gmail.com>
2020-12-11 10:01:23 +0530

    bug fixed

--- src/App.js
+++ src/App.js
@@ -30,59 +30,59 @@ function App() {
   // Check is winner
   function checkIsWinner()
   {
-    if(itemArray[0] == itemArray[1] &&
-       itemArray[0] == itemArray[2] &&
+    if(itemArray[0] === itemArray[1] &&
+       itemArray[0] === itemArray[2] &&
        itemArray[0] !== "empty")
     {
        setWinMessage(`${itemArray[0]} wins`)
     }
     else if(
       itemArray[3] !== "empty" &&
-      itemArray[3] == itemArray[4]&&
-      itemArray[4] == itemArray[5])
+      itemArray[3] === itemArray[4]&&
+      itemArray[4] === itemArray[5])
     {
       setWinMessage(`${itemArray[3]} wins`)
     }
     else if(
       itemArray[6] !== "empty" &&
-      itemArray[6] == itemArray[7]&&
-      itemArray[7] == itemArray[8]
+      itemArray[6] === itemArray[7]&&
+      itemArray[7] === itemArray[8]
       )
     {
       setWinMessage(`${itemArray[6]} wins`)
     }
     else if(
       itemArray[0] !== "empty" &&
-      itemArray[0] == itemArray[3]&&
-      itemArray[3] == itemArray[6])
+      itemArray[0] === itemArray[3]&&
+      itemArray[3] === itemArray[6])
     {
       setWinMessage(`${itemArray[0]} wins`)
     }
     else if(
       itemArray[1] !== "empty" &&
-      itemArray[1] == itemArray[4]&&
-      itemArray[4] == itemArray[7])
+      itemArray[1] === itemArray[4]&&
+      itemArray[4] === itemArray[7])
     {
       setWinMessage(`${itemArray[1]} wins`)
     }
     else if(
       itemArray[2] !== "empty" &&
-      itemArray[2] == itemArray[5]&&
-      itemArray[5] == itemArray[8])
+      itemArray[2] === itemArray[5]&&
+      itemArray[5] === itemArray[8])
     {
       setWinMessage(`${itemArray[2]} wins`)
     }
     else if(
       itemArray[0] !== "empty" &&
-      itemArray[0] == itemArray[4]&&
-      itemArray[4] == itemArray[8])
+      itemArray[0] === itemArray[4]&&
+      itemArray[4] === itemArray[8])
     {
       setWinMessage(`${itemArray[0]} wins`)
     }
     else if(
       itemArray[2] !== "empty" &&
-      itemArray[2] == itemArray[4]&&
-      itemArray[4] == itemArray[6])
+      itemArray[2] === itemArray[4]&&
+      itemArray[4] === itemArray[6])
     {
       setWinMessage(`${itemArray[2]} wins`)
     }
@@ -93,7 +93,7 @@ function App() {
     if(winMessage){
       return toast(winMessage, { type:"success"})
     }
-    if(itemArray[itemNumber] == "empty"){
+    if(itemArray[itemNumber] === "empty"){
        itemArray[itemNumber] = (isCross) ? "cross":"circle";
        setIsCross(!isCross);
     }
